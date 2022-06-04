ebd78a8cb9bec9aac9d12412905f47b67dcd5f7d
maxime ngoe <maxime.ngoe@gmail.com>
2022-06-04 10:47:52 +0100

    creation nouveau fichier

---
+++ cs/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+from core import models as core_models
+
+class ChequeImport(models.Model):
+    id = models.AutoField(db_column="ChequeImportID", primary_key=True)
+    importDate = models.DateField()
+    user = models.ForeignKey(
+        core_models.InteractiveUser, models.DO_NOTHING, db_column="UserID"
+    )
+    stored_file = models.FileField(upload_to="csImports/%Y/%m/", db_column="ImportFile",default="")
+
+    class Meta:
+        db_table = "tblChequeSanteImport"
