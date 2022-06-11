a56bea9c0b0d71fe7d1d66554530996985e56f80
maxime ngoe <maxime.ngoe@gmail.com>
2022-06-11 11:52:01 +0100

    fix Typo models.py

--- cs/models.py
+++ cs/models.py
@@ -3,7 +3,10 @@ from django.db import models
 from core import models as core_models
 
 class ChequeImport(models.Model):
-    id = models.AutoField(db_column="ChequeImportID", primary_key=True)
+    """ Class Cheque Import : 
+    Class for importation of check in the system
+    """
+    idChequeImport = models.AutoField(db_column="ChequeImportID", primary_key=True)
     importDate = models.DateTimeField()
     user = models.ForeignKey(
         core_models.InteractiveUser, models.DO_NOTHING, db_column="UserID"
@@ -11,22 +14,32 @@ class ChequeImport(models.Model):
     stored_file = models.FileField(
         upload_to="csImports/%Y/%m/",
         db_column="ImportFile",
-        default="", 
+        default="",
         null=True,
         blank=True
     )
 
+    """ Class Meta : 
+    Class Meta to define specific table
+    """
     class Meta:
         db_table = "tblChequeSanteImport"
 
 
 
 class ChequeImportLine(models.Model):
-    id = models.AutoField( primary_key=True)
+    """ Class Cheque Import Line : 
+    Class to save parsed CSV file uploaded and save all insert / update
+    """
+    idChequeImportLine = models.AutoField( primary_key=True)
     chequeImportId = models.ForeignKey(ChequeImport, models.DO_NOTHING)
     chequeImportLineCode = models.CharField(max_length=100)
     chequeImportLineDate = models.DateTimeField()
     chequeImportLineStatus = models.CharField(max_length=50)
 
+
+    """ Class Meta : 
+    Class Meta to define specific table
+    """
     class Meta:
         db_table ='tblChequeSanteImportLine'
