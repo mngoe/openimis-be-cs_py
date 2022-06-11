65a196a4433cb30ff70f437f43eba3cf4c86effd
maxime ngoe <maxime.ngoe@gmail.com>
2022-06-11 11:16:54 +0100

    ajustement model + suppression de cache inutile

--- cs/models.py
+++ cs/models.py
@@ -4,7 +4,7 @@ from core import models as core_models
 
 class ChequeImport(models.Model):
     id = models.AutoField(db_column="ChequeImportID", primary_key=True)
-    importDate = models.DateField()
+    importDate = models.DateTimeField()
     user = models.ForeignKey(
         core_models.InteractiveUser, models.DO_NOTHING, db_column="UserID"
     )
@@ -18,3 +18,15 @@ class ChequeImport(models.Model):
 
     class Meta:
         db_table = "tblChequeSanteImport"
+
+
+
+class ChequeImportLine(models.Model):
+    id = models.AutoField( primary_key=True)
+    chequeImportId = models.ForeignKey(ChequeImport, models.DO_NOTHING)
+    chequeImportLineCode = models.CharField(max_length=100)
+    chequeImportLineDate = models.DateTimeField()
+    chequeImportLineStatus = models.CharField(max_length=50)
+
+    class Meta:
+        db_table ='tblChequeSanteImportLine'
